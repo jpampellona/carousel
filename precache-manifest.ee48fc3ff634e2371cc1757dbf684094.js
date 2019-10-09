a65bf8bedb41bbda5f89b539722ce113662e9c67
Jay-R Pampellona <jr.pampellona@gmail.com>
2019-10-10 00:01:02 +0800

    Updates

---
+++ precache-manifest.ee48fc3ff634e2371cc1757dbf684094.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cab9d7887b2b9424b801e2139e282a2",
+    "url": "/carousel/index.html"
+  },
+  {
+    "revision": "1793d93dc8df243c4cc0",
+    "url": "/carousel/static/css/main.52fba766.chunk.css"
+  },
+  {
+    "revision": "87022ccabb655c726098",
+    "url": "/carousel/static/js/2.7d8182ca.chunk.js"
+  },
+  {
+    "revision": "1793d93dc8df243c4cc0",
+    "url": "/carousel/static/js/main.50b38bc9.chunk.js"
+  },
+  {
+    "revision": "9237c9479315b9a9ad34",
+    "url": "/carousel/static/js/runtime-main.a3ae819f.js"
+  }
+]);
